2aed16b31fdd2e898192d8a2bd01f02e661d278d
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-07 20:36:02 -0300

    feat: create project - strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ccdb275dfc907974c5382881971b0672'),
+    },
+  },
+});
